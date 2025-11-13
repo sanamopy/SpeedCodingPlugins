090f3b9f20ee46f0c51c09aa52a0a15fc5aaebd4
sanamo <kartersanamo05@gmail.com>
2025-11-13 13:31:00 -0600

    Create FreezeArrow project

---
+++ FreezeArrow/src/main/java/com/sanamo/freezeArrow/FreezeArrow.java
@@ -0,0 +1,17 @@
+package com.sanamo.freezeArrow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FreezeArrow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
