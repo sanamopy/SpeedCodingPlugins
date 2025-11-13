3cf200027ec73a90459b1c85845a44a4560b4092
sanamo <kartersanamo05@gmail.com>
2025-11-13 12:59:54 -0600

    Added the AutoSmelt Command

---
+++ AutoSmelt/src/main/java/com/sanamo/autoSmelt/Commands/AutoSmelt.java
@@ -0,0 +1,12 @@
+package com.sanamo.autoSmelt.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class AutoSmelt implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+}
